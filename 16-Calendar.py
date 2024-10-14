87021e24f820a31c65abd0c0312a43b7375413be
Talhak <talhawkk@gmail.com>
2024-10-15 00:05:57 +0500

    complete program

---
+++ 16-Calendar.py
@@ -0,0 +1,7 @@
+#Calendar
+import calendar
+year=int(input("\nenter year:"))
+# For Month
+# month=int(input("enter month"))
+# print(calendar.month(year,month))
+print(calendar.calendar(year))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+#Calendar
+import calendar
+month=int(input("enter month"))
+year=int(input("\nenter year:"))
+print(calendar.calendar(year))
