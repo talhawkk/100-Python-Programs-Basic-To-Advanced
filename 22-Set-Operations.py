7f37a21284d0178feb7e531ec71f480d4f8f58f4
Talhak <talhawkk@gmail.com>
2024-10-16 00:22:15 +0500

    complete program

---
+++ 22-Set-Operations.py
@@ -0,0 +1,8 @@
+#performing Sets operation:
+A={23,254,23,34}
+B={34,23,53,5,34}
+
+print ("The Union is",A|B)
+print ("The Intersection is", A & B)
+print ("The Difference is",A-B)
+print ("The Symmetric difference is",A^B)
