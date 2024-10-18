b54a166ab9da66bcc19f1634ba3fdb5ad75c7cdb
Talhak <talhawkk@gmail.com>
2024-10-18 19:57:32 +0500

    complete program

---
+++ 47-Copy-contents-from-onefile-to-another.py
@@ -0,0 +1,4 @@
+#How to Copy Contents of One File to Another File
+from shutil import copyfile
+copyfile("C:/Users/Talha/Desktop/Python/talha.txt","C:/Users/Talha/Desktop/Python/hamza.txt")
+#First parameter : main file, second parameter : in which you want to paste
