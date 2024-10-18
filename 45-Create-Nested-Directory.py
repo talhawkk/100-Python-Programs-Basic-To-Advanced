567dea32cfdb40fde61ac97a166767ef0dbd41fb
Talhak <talhawkk@gmail.com>
2024-10-18 19:51:34 +0500

    complete program

---
+++ 45-Create-Nested-Directory.py
@@ -0,0 +1,4 @@
+#how to create a nested directory:
+from pathlib import Path
+Path("C:/Users/Talha/Desktop/Python/blur/interstellar/mango/man").mkdir(parents=True,exist_ok=True)
+#copy the path than change all backward slashed into forward slashed and than write name of folder or folders after path
