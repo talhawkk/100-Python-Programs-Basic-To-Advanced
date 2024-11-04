42d5a2d8949f839ad2c8c0fa99693490fe253640
Talhak <talhawkk@gmail.com>
2024-11-04 19:00:28 +0500

    complete program
    using three modules and take_command function form previous code of53

---
+++ 62-Urdu-to-English.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import pyaudio
+from deep_translator import GoogleTranslator
+import pyttsx3
+
+def take_command():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold=.8
+        audio=r.listen(source)
+        try:
+            query=r.recognize_google(audio,language='ur-pk')
+            print(f"User said: {query}")
+            return query
+        except:
+            print("finding trouble in Recognizing....")
+
+while True:
+    query=take_command()
+    translator = GoogleTranslator(source='ur', target='en')
+    translation = translator.translate(query)
+    print(translation)
+    engine = pyttsx3.init()
+
+    engine.say(translation)
+
+    engine.runAndWait()
+
